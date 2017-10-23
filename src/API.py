f6e9579b8d302309ae888026fe801ee358796caa
Floikk <marijn.j.a.boer@gmail.com>
2017-10-23 17:02:10 +0200

    Version 0.1

--- src/API.py
+++ src/API.py
@@ -14,54 +14,68 @@ AW_arguments = {
     'subfamily':    'formicinae',
     'caste':        'worker',
     'img_type':     'h',
-    'limit':        25
-    }
-AW_base_url = 'http://www.antweb.org/api/v2/?'
+    'limit':        100
+}
 
-AW_url = requests.get(AW_base_url, AW_arguments)
+AW_base_url = 'http://www.antweb.org/api/v2/?'
+AW_url = requests.get(url=AW_base_url, params=AW_arguments)
 
 def get_url_info(input_url):
     print("URL:", input_url.url)
     print("URL headers:", input_url.headers)
     print("URL type:", type(input_url.content))
     print("Connection status:", input_url.status_code)
+
     if input_url.status_code == 200:
         print("Request: the request was fulfilled.")
+
     else:
         print("Request: the request was not fulfilled.")
 
     print("Time elapsed to connect to URL:", input_url.elapsed)
 
-
 get_url_info(AW_url)
 
-
-
 def get_json(input_url):
-    return input_url.json()
-
-AW_json = get_json(AW_url)
-
-def print_urls(x):
-    url_dict = {}
-    for specimen in x["specimens"]:
-            print(specimen.get("scientific name"))
+    AW_data = json.loads(input_url.text)
+    return AW_data
 
 
-print_urls(AW_json)
+AW_data_json = get_json(AW_url)
+# print(AW_data_json)
 
 
-#   json format     [dict]   [lst]  [dict] [dict]   [dict]    [dict][dict][lst]
-image_test = (my_dict['specimens'][0]['images']['1']['shot_types']['p']['img'][0:3])
+def print_urls(AW_data_json):
+
+    AW_dict = {}
+
+    # loop through all specimens
+    for x in AW_data_json["specimens"]:
+
+        #if the specimen has images, continue
+        if "images" in x:
+
+            #find the thumbview link for the head image
+            for image_head in x['images']['1']['shot_types']['h']['img']:
+                if "thumbview" in image_head:
+                    print(image_head)
+
+            #find the thumbview link for the dorsal image
+            for image_dorsal in x['images']['1']['shot_types']['d']['img']:
+                if "thumbview" in image_dorsal:
+                    print(image_dorsal)
+
+            #find the thumbview link for the profile image
+            for image_profile in x['images']['1']['shot_types']['p']['img']:
+                if "thumbview" in image_profile:
+                    print(image_profile)
+    AW_dict = AW_dict + {"scientific_name" : "image_head"}
+
+print_urls(AW_data_json)
 
 
 
-
-
-
-
-
-
+################################################################################
 
 def download_photo(self, img_url, filename):
     try:
@@ -80,7 +94,9 @@ def download_photo(self, img_url, filename):
         return False
     return True
 
-download_photo('http://www.AW.org/images/casent0005326/casent0005326_d_1_high.jpg', 'test_image.jpg')
+
+download_photo(
+    'http://www.AW.org/images/casent0005326/casent0005326_d_1_high.jpg', 'test_image.jpg')
 
 
 for i in my_dict:
@@ -96,31 +112,32 @@ Note the / in the get method.
 Unfortunately, this is not possible in vanilla Python, but with a really small helper class you can easily make this happen:
 """
 
+
 class DictQuery(dict):
-    def get(self, path, default = None):
+    def get(self, path, default=None):
         keys = path.split("/")
         val = None
 
         for key in keys:
             if val:
                 if isinstance(val, list):
-                    val = [ v.get(key, default) if v else None for v in val]
+                    val = [v.get(key, default) if v else None for v in val]
                 else:
                     val = val.get(key, default)
             else:
                 val = dict.get(self, key, default)
 
             if not val:
-                break;
+                break
 
         return val
 
 
-
 for item in my_dict:
-    print (DictQuery(item).get("specimens//images/1/shot_types/p"))
+    print(DictQuery(item).get("specimens//images/1/shot_types/p"))
 
 #   json format     [dict]   [lst]  [dict] [dict]   [dict]    [dict][dict][lst]
-image_test = (my_dict['specimens'][0]['images']['1']['shot_types']['p']['img'][0:3])
+image_test = (my_dict['specimens'][0]['images']
+              ['1']['shot_types']['p']['img'][0:3])
 
 print(image_test)
