04c55fe617dbafe96ab389abe3ed1c8435ce7845
marijnjaboer <marijn.j.a.boer@gmail.com>
2018-02-07 15:35:35 +0100

    first trial to update AW_json for creating urls and downloading json

--- formicID/AntWeb/AW_to_json.py
+++ formicID/AntWeb/AW_to_json.py
@@ -15,24 +15,22 @@ file for this genus+species and will then create an csv file containing a
 
 import requests
 import json
-import jmespath
 import pandas as pd
 import time
 import datetime
 import os
-import csv
-from functools import wraps
-from urllib.request import urlretrieve
 from tqdm import tqdm
 
 
 # Parameters and settings
 # //////////////////////////////////////////////////////////////////////////////
 todaystr = datetime.date.today().isoformat()
-
+wd = os.getcwd()
 
 # AntWeb basic information
 # //////////////////////////////////////////////////////////////////////////////
+
+
 def create_url(limit, offset, **kwargs):
     """
     # Description:
@@ -65,8 +63,8 @@ def create_url(limit, offset, **kwargs):
         'offset':       offset,
         'genus':        genus,
         'species':      species,
-        'country':      country
-        'caste':        caste # not working
+        'country':      country,
+        'caste':        caste  # not working
     }
     # Creating the AntWeb url from the base url and the API arguments
     url = requests.get(url=base_url, params=arguments)
@@ -115,103 +113,11 @@ def get_json(input_url):
     else:
         return data
 
-
-def max_specimens(json):
-    """
-    # Description:
-        View the maximum number of specimens specified in an AntWeb JSON object.
-
-    # Input:
-        json = A JSON object
-
-    # Returns:
-        An integer of the maximum number of specimens in a JSON object.
-    """
-    nb_specimens = json["count"]
-    return int(nb_specimens)
-
-
-def filter_json(json_file):
-    """
-    # Description:
-        Load a JSON object and filter for only relevant values.
-
-    # Input:
-        json_file = path to a JSON file
-
-    # Returns:
-        A list of lists with 4 values
-
-    # TODO:
-        combine with create() ?
-    """
-    json_txt = json.load(json_file)
-    data_filtered = jmespath.search('specimens[].[catalogNumber,'
-                                    'scientific_name, images."1".shot_types]',
-                                    json_txt)
-
-    lst = []
-    for row in data_filtered:
-        if row[2] != None:
-            # print(row)
-            catalog_number = row[0]
-            scientific_name = row[1]
-            image_url = {}
-            if 'h' in row[2]:
-                image_url['h'] = row[2]['h']['img'][1]
-            if 'p' in row[2]:
-                image_url['p'] = row[2]['p']['img'][1]
-            if 'd' in row[2]:
-                image_url['d'] = row[2]['d']['img'][1]
-            for key in image_url:
-                new_row = [catalog_number,
-                           scientific_name, key, image_url[key]]
-                lst.append(new_row)
-
-    return lst
-
-
-def create(lst):
-    """
-    # Description:
-        Creates a pandas dataframe with 4 specific column names from the list created by the filter_json() function
-
-    # Input:
-        A list of lists containing 4 items, corresponding to the columnnames
-
-    # Returns:
-        A 4 column pandas dataframe
-
-    """
-    columns = ['catalog_number', 'scientific_name', 'shot_type', 'image_url']
-    df = pd.DataFrame(columns=columns)
-    df = pd.DataFrame(lst)
-
-    return df
-
-
-def open_csv(path_to_csv):
-    """
-    # Description:
-        This function opens a csv file and stores it in a pandas dataframe
-
-    # Input:
-        path_to_csv = The 'pathway' to a .csv file with header, seperated by comma
-
-    # Returns:
-        A pandas dataframe created from a .csv file
-
-    # TODO:
-        Fix relative / absolute pathways. What to use?
-    """
-    df = pd.read_csv(path_to_csv, sep=';', header=0)
-    return df
-
 # Executing
 # //////////////////////////////////////////////////////////////////////////////
 
 
-def urls_to_json(csv_species, offset_set, limit_set):
+def urls_to_json(csv_species, output_dir, offset_set, limit_set):
     """
     # Description:
         This function downloads JSON files for a list of species and places them in a drecitory.
@@ -228,7 +134,8 @@ def urls_to_json(csv_species, offset_set, limit_set):
         Fix pathways
         Add directory as argument
     """
-    df_top101 = open_csv(csv_species)
+    csv_species = os.path.join(wd, csv_species)
+    df_top101 = pd.read_csv(csv_species, sep=';', header=0)
 
     offset = offset_set
     limit = limit_set
@@ -236,6 +143,7 @@ def urls_to_json(csv_species, offset_set, limit_set):
     # 630976 / 9859 = 64
     check = limit - offset
 
+    # TODO: output_dir
     path = './data/top101-JSON/'
     if not os.path.exists(path):
         os.mkdir(path)
@@ -265,15 +173,9 @@ def urls_to_json(csv_species, offset_set, limit_set):
         time.sleep(0.5)
 
 
-
 if __name__ == '__main__':
-
-    # urls_to_json(
-    #     csvfile='./data/2018-01-09-db-Top101imagedspecies.csv',
-    #     offset_set=0,
-    #     limit_set=6000
-    # )
-
-    batch_filter_to_csv(
-        directory='./data/top101-JSON'
+    urls_to_json(
+        csv_species='./data/2018-01-09-db-Top101imagedspecies.csv',
+        offset_set=0,
+        limit_set=6000
     )
