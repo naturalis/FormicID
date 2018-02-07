096587a213c7323e009b9374e968212fd5cd3387
marijnjaboer <marijn.j.a.boer@gmail.com>
2018-02-07 11:12:53 +0100

    models as function with description

--- formicID/models.py
+++ formicID/models.py
@@ -1,81 +1,107 @@
 ################################################################################
 #                                                                              #
-#                        FormicID Build and Compile                            #
+#                        FormicID model imports                                #
 #                                                                              #
 ################################################################################
 
 # Packages
 # //////////////////////////////////////////////////////////////////////////////
-from keras.models import Sequential  # for creating the model
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras.optimizers import SGD, RMSprop, Adam, Nadam
-
-
+from keras.applications.resnet50 import ResNet50  # ResNet based
+from keras.applications.densenet import DenseNet169  # ResNet based
+from keras.applications.inception_v3 import InceptionV3  # Inception based
+from keras.applications.xception import Xception  # Inception based
 
 # Parameters and settings
 # //////////////////////////////////////////////////////////////////////////////
 
 
-# Build the network
+# Model Settings
 # //////////////////////////////////////////////////////////////////////////////
-class neuralNetwork(object):
-    """
-    A neural netowrk model
+'''All of these architectures (except Xception and MobileNet) are compatible with both TensorFlow and Theano, and upon instantiation the models will be built according to the image data format set in your Keras configuration file at  ~/.keras/keras.json. For instance, if you have set image_data_format=channels_last, then any model loaded from this repository will get built according to the TensorFlow data format convention, "Height-Width-Depth". The Xception model is only available for TensorFlow, due to its reliance on SeparableConvolution layers.'''
 
-    Attributes:
-        input_shape: a tuple of img_height, img_width and 3 channels
-        dropout: a percentage of dropout
-        num_species: a integer of the number of species
-        optimizer: either 'SGD', 'RMSprop' or 'Nadam'.
+# ResNet50
+# //////////////////////////////////////////////////////////////////////////////
 
-    """
-    def __init__(self, dropout, input_shape, num_species, optimizer):
-        """
-        # Returns
-            A neural network model. The input_shape needs to be specified
-        """
-        self.dropout = dropout
-        self.input_shape = input_shape
-        self.num_species = num_species
-        self.optimizer = optimizer
 
-    def build_neural_network(self):
-        """
-        # Returns
-            A keras model.
-        """
-        model = Sequential()
-        model.add(Conv2D(32, (3, 3), padding='same',
-                         input_shape=self.input_shape))
-        model.add(Activation('relu'))
-        model.add(Conv2D(32, (3, 3)))
-        model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(self.dropout))
-        model.add(Conv2D(64, (3, 3), padding='same'))
-        model.add(Activation('relu'))
-        model.add(Conv2D(64, (3, 3)))
-        model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(self.dropout))
-        model.add(Flatten())
-        model.add(Dense(512))
-        model.add(Activation('relu'))
-        model.add(Dropout(self.dropout))
-        model.add(Dense(self.num_species))
-        model.add(Activation('softmax'))  # or use svm?
+def model_resnet50(classes, weights, input_tensor, input_shape, pooling, classes):
+    '''
+    ResNet50 model, with weights pre-trained on ImageNet.
 
-        if self.optimizer == "SGD":
-            opt = SGD(lr=1e-2, decay=1e-6, momentum=0.9, nesterov=True)
-        if self.optimizer == "RMSprop":
-            opt = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
-        if self.optimizer == "Nadam":
-            opt = Nadam(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08,
-            schedule_decay=0.004)
+    This model is available for both the Theano and TensorFlow backend, and can be built both with 'channels_first' data format (channels, height, width) or 'channels_last' data format (height, width, channels).
 
-        model.compile(loss='categorical_crossentropy',
-                      optimizer=opt,
-                      metrics=['accuracy', 'top_k_categorical_accuracy'])
-        print("Model was build and compiled succesfully.")
-        return model
+    The default input size for this model is 224x224.
+    '''
+    model = ResNet50(
+        include_top=include_top,
+        weights=weights,
+        input_tensor=input_tensor,
+        input_shape=input_shape,
+        pooling=pooling,
+        classes=classes)
+    return model
+
+
+# DenseNet169
+# //////////////////////////////////////////////////////////////////////////////
+
+
+def model_densenet169(classes, weights, input_tensor, input_shape, pooling, classes):
+    '''
+    Optionally loads weights pre-trained on ImageNet. Note that when using TensorFlow, for best performance you should set image_data_format='channels_last' in your Keras config at ~/.keras/keras.json.
+
+    The model and the weights are compatible with TensorFlow, Theano, and CNTK. The data format convention used by the model is the one specified in your Keras config file.
+    '''
+    model = DenseNet169(
+        include_top=include_top,
+        weights=weights,
+        input_tensor=input_tensor,
+        input_shape=input_shape,
+        pooling=pooling,
+        classes=classes)
+    return model
+
+
+# InceptionV3
+# //////////////////////////////////////////////////////////////////////////////
+
+
+def model_inceptionv3(classes, weights, input_tensor, input_shape, pooling, classes):
+    '''
+    Inception V3 model, with weights pre-trained on ImageNet.
+
+    This model is available for both the Theano and TensorFlow backend, and can be built both with 'channels_first' data format (channels, height, width) or 'channels_last' data format (height, width, channels).
+
+    The default input size for this model is 299x299.
+    '''
+    model = InceptionV3(
+        include_top=include_top,
+        weights=weights,
+        input_tensor=input_tensor,
+        input_shape=input_shape,
+        pooling=pooling,
+        classes=classes)
+    return model
+
+
+# Xception
+# //////////////////////////////////////////////////////////////////////////////
+
+
+def model_xception(classes, weights, input_tensor, input_shape, pooling, classes):
+    '''
+    Xception V1 model, with weights pre-trained on ImageNet.
+
+    On ImageNet, this model gets to a top-1 validation accuracy of 0.790 and a top-5 validation accuracy of 0.945.
+
+    Note that this model is only available for the TensorFlow backend, due to its reliance on SeparableConvolution layers. Additionally it only supports the data format 'channels_last' (height, width, channels).
+
+    The default input size for this model is 299x299.
+    '''
+    model = Xception(
+        include_top=include_top,
+        weights=weights,
+        input_tensor=input_tensor,
+        input_shape=input_shape,
+        pooling=pooling,
+        classes=classes)
+    return model
