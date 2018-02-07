1a8c263127a8afb7431d5c3b2318c9eda3b0fa25
marijnjaboer <marijn.j.a.boer@gmail.com>
2018-02-07 15:21:28 +0100

    filter json files in folder to csv output fix

--- formicID/AntWeb/json_to_csv.py
+++ formicID/AntWeb/json_to_csv.py
@@ -28,18 +28,58 @@ from tqdm import tqdm
 
 # Parameters and settings
 # //////////////////////////////////////////////////////////////////////////////
-todaystr = datetime.date.today().isoformat()
+wd = os.getcwd()
 
-
-# Batch batch_filter_to_csv
+# Filter
 # //////////////////////////////////////////////////////////////////////////////
-def batch_filter_to_csv(directory):
+def filter_json(json_file):
     """
     # Description:
-        Download all images found from a batch of JSON files in a directory
+        Load a JSON object and filter for only relevant values.
 
     # Input:
-        directory = path and name of the output directory
+        json_file = path to a JSON file
+
+    # Returns:
+        A list of lists with 4 values
+
+    # TODO:
+        combine with create() ?
+    """
+    json_txt = json.load(json_file)
+    data_filtered = jmespath.search('specimens[].[catalogNumber,'
+                                    'scientific_name, images."1".shot_types]',
+                                    json_txt)
+
+    lst = []
+    for row in data_filtered:
+        if row[2] != None:
+            # print(row)
+            catalog_number = row[0]
+            scientific_name = row[1]
+            image_url = {}
+            if 'h' in row[2]:
+                image_url['h'] = row[2]['h']['img'][1]
+            if 'p' in row[2]:
+                image_url['p'] = row[2]['p']['img'][1]
+            if 'd' in row[2]:
+                image_url['d'] = row[2]['d']['img'][1]
+            for key in image_url:
+                new_row = [catalog_number,
+                           scientific_name, key, image_url[key]]
+                lst.append(new_row)
+
+    return lst
+# Batch batch_filter_to_csv
+# //////////////////////////////////////////////////////////////////////////////
+def batch_filter_to_csv(input_dir, output_dir, csvname):
+    """
+    # Description:
+        from a json or batch of json files a csvfile is created with relevant
+        information for downloading the images and naming the files
+
+    # Input:
+        output_dir = path and name of the output directory
 
     # Returns:
         creates a .csv file in the output directory
@@ -47,29 +87,37 @@ def batch_filter_to_csv(directory):
     # TODO:
         create input for:
         - the input directory
-        - output directory
+        - output_dir
         - filename
     """
+    suffix = '.csv'
+    input_dir = os.path.join(wd, input_dir)
     df2 = pd.DataFrame()
-    for filename in tqdm(os.listdir(directory),
+
+    columns = ['catalog_number', 'scientific_name', 'shot_type', 'image_url']
+
+    for filename in tqdm(os.listdir(input_dir),
                          desc='Reading JSON files'):
         if filename.endswith('.json'):
-            with open(os.path.join(directory, filename)) as data_file:
+            with open(os.path.join(input_dir, filename)) as data_file:
                 # print('Filtering {}'.format(filename))
                 lst = filter_json(data_file)
-                df = create(lst)
+                df = pd.DataFrame(lst, columns=columns)
                 df2 = df2.append(df)
 
     # replace spaces between genus and species names with underscores
     df2.replace('\s+', '_', regex=True, inplace=True)
-    df2.columns = [
-        'catalog_number',
-        'scientific_name',
-        'shot_type',
-        'image_url'
-    ]
+    df2.columns = columns
     # file_path = os.path.join(path, file_name)
-    path = './data/top101-JSON/'
-    file_name = 'top101.csv'
-    df2.to_csv(os.path.join(path, file_name), index=False, header=True)
-    print('All JSON files are read, filtered and added to the csv file.')
+    output_dir = os.path.join(wd, output_dir)
+    df2.to_csv(os.path.join(output_dir, csvname + suffix), index=False, header=True)
+    print('All JSON files are read, filtered and added to the csv file. \n'
+    '{}{} was created in {}'.format(csvname, suffix, output_dir))
+
+
+if __name__ == '__main__':
+    batch_filter_to_csv(
+        input_dir='data/top101-JSON',
+        output_dir='data/top101-JSON',
+        csvname='test'
+    )
