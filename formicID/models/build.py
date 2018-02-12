8f6c3ecba236662046941188eeb4fdfecaaafed9
marijnjaboer <marijn.j.a.boer@gmail.com>
2018-02-12 12:20:13 +0100

    split compile and build def

--- formicID/models/build.py
+++ formicID/models/build.py
@@ -19,8 +19,6 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.optimizers import SGD, RMSprop, Adam, Nadam
 
-
-
 # Parameters and settings
 # //////////////////////////////////////////////////////////////////////////////
 
@@ -28,30 +26,30 @@ from keras.optimizers import SGD, RMSprop, Adam, Nadam
 # Build the network
 # //////////////////////////////////////////////////////////////////////////////
 class neuralNetwork(object):
-    """
-    A neural netowrk model
-
-    Attributes:
-        input_shape: a tuple of img_height, img_width and 3 channels
-        dropout: a percentage of dropout
-        num_species: a integer of the number of species
-        optimizer: either 'SGD', 'RMSprop' or 'Nadam'.
-
-    """
     def __init__(self, dropout, input_shape, num_species, optimizer):
-        """
-        # Returns
-            A neural network model. The input_shape needs to be specified
+        """A neural network model. The input_shape needs to be specified.
+
+        Args:
+            dropout (type): a percentage of dropout.
+            input_shape (type): image height, width and depth.
+            num_species (type): an integer of the number of species.
+            optimizer (type): either 'SGD', 'RMSprop' or 'Nadam'.
+
+        Returns:
+            type: Description of returned object.
+
         """
         self.dropout = dropout
         self.input_shape = input_shape
         self.num_species = num_species
         self.optimizer = optimizer
 
-    def build_neural_network(self):
-        """
-        # Returns
-            A keras model.
+    def build(self):
+        """Short summary.
+
+        Returns:
+            type: Description of returned object.
+
         """
         model = Sequential()
         model.add(Conv2D(32, (3, 3), padding='same',
@@ -72,18 +70,26 @@ class neuralNetwork(object):
         model.add(Activation('relu'))
         model.add(Dropout(self.dropout))
         model.add(Dense(self.num_species))
-        model.add(Activation('softmax'))  # or use svm?
+        model.add(Activation('softmax'))
+        print("Model is build succesfully.")
 
+    def compile(self):
+        """Short summary.
+
+        Returns:
+            type: Description of returned object.
+
+        """
         if self.optimizer == "SGD":
             opt = SGD(lr=1e-2, decay=1e-6, momentum=0.9, nesterov=True)
         if self.optimizer == "RMSprop":
             opt = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
         if self.optimizer == "Nadam":
             opt = Nadam(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08,
-            schedule_decay=0.004)
+                        schedule_decay=0.004)
 
         model.compile(loss='categorical_crossentropy',
                       optimizer=opt,
                       metrics=['accuracy', 'top_k_categorical_accuracy'])
-        print("Model was build and compiled succesfully.")
+        print("Model is compiled succesfully.")
         return model
