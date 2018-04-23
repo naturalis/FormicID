6439bda69b998b4dd1537de476a5cefd6c601237
marijnjaboer <marijn.j.a.boer@gmail.com>
2018-04-23 13:53:42 +0200

    rm linebreak

--- formicID/AntWeb/AW3_to_json.py
+++ formicID/AntWeb/AW3_to_json.py
@@ -40,6 +40,7 @@ from utils.utils import wd
 # Creating an URL
 ###############################################################################
 
+
 def create_url(limit, offset, **kwargs):
     """Creation of the url to access AntWebs API V3, using a base_url and
     arguments.
